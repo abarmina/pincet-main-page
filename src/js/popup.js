f19a2c2c19fba65662cc1cacbf9b57f77d58673f
abarmina <anutik-barmina@mail.ru>
2020-07-20 01:20:06 +0500

    fixed item units

---
+++ src/js/popup.js
@@ -0,0 +1,11 @@
+// const popup = document.getElementById('j-popup'),
+//     popupCheckinButton = document.querySelector('j-checkin-button'),
+//     popupCancelButton = document.getElementById('j-cancel-button');
+
+// popupDeleteButton.addEventListener('click', () => {
+//     popup.classList.add('j-active');
+// });
+
+// popupCancelButton.addEventListener('click', () => {
+//     popup.classList.remove('j-active');
+// })
